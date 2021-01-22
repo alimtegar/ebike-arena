16d318c74bf5ba73717e56469af23ab534c9338d
alimtegar <alimtegar4@gmail.com>
2021-01-22 08:52:49 +0700

    Create responsive navbar search

--- components/Navbar/NavbarMenu.js
+++ components/Navbar/NavbarMenu.js
@@ -1,28 +1,34 @@
+import { useState } from 'react';
 import createPersistedState from 'use-persisted-state';
-
 import Link from '../Link';
-import { ShoppingCartIcon, SearchIcon, ChevronDownIcon } from '../Icons';
+import NavbarSearch from './NavbarSearch';
+import { ShoppingCartIcon, SearchIcon, XIcon, ChevronDownIcon } from '../Icons';
 
+// Helpers
 import { getTotalAmount } from '../../helpers';
 
 const useCartState = createPersistedState('cart');
 
 const NavbarMenu = () => {
     const [cart, _] = useCartState([]);
+    const [showNavbar, setShowNavbar] = useState(false);
 
     return (
         <div className="flex">
+            {/* Cart */}
             {/* <div > */}
-                <Link href="/cart">
-                    <a className="flex items-center font-bold ml-6">
-                        <ShoppingCartIcon width={5} height={5} />
+            <Link href="/cart">
+                <a className="flex items-center font-bold ml-6">
+                    <ShoppingCartIcon width={5} height={5} />
 
-                        <span className="hidden md:flex ml-1">Cart</span>
+                    <span className="hidden md:flex ml-1">Cart</span>
 
-                        <sup className="font-semibold text-sm ml-1">{getTotalAmount(cart)}</sup>
-                    </a>
-                </Link>
+                    <sup className="font-semibold text-sm ml-0.5 md:ml-1">{getTotalAmount(cart)}</sup>
+                </a>
+            </Link>
             {/* </div> */}
+
+            {/* Products */}
             <div className="navbar-menu-item-products relative hidden md:flex items-center font-bold ml-4">
                 <Link href="/products">
                     <a>Products</a>
@@ -32,9 +38,27 @@ const NavbarMenu = () => {
                     <ChevronDownIcon width={5} height={5} color="gray-400" />
                 </span>
             </div>
-            <div className="flex md:hidden items-center font-bold ml-6 mr-2">
-                <SearchIcon width={5} height={5} />
-            </div>
+
+            {/* Searh toggle */}
+            <button 
+                type="button"
+                className="flex md:hidden items-center font-bold ml-6 mr-2 focus:outline-none"
+                onClick={() => setShowNavbar(!showNavbar)}
+            >
+                {showNavbar ? (
+                    <XIcon width={5} height={5} />
+                ) : (
+                    <SearchIcon width={5} height={5} />
+                )}
+            </button>
+
+            {showNavbar ? (
+                <div className="absolute top-16 left-0 bg-white w-screen shadow">
+                    <div className="px-3 pb-3">
+                        <NavbarSearch />
+                    </div>
+                </div>
+            ) : null}
         </div>
     );
 };
